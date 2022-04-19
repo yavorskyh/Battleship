57b3af4e3bcd5497a974bd25aa30dffca7fdfb58
Yvonne <yjrodriguez16@gmail.com>
2022-04-18 21:38:51 -0400

    gameboard and placeships
    fixed gameboard
    moved placeships to player file
    moved printgameboard to player file

--- Battleship.java
+++ Battleship.java
@@ -1,50 +1,25 @@
+import java.util.*;
 
 public class Battleship {
-	
+
 	// main loop (initialize game board, have each player place ships, then loop through
 	// printGameBoard and attack for each player until a winner is decided)
 	public static void main(String[] args) {
 		Player player1 = new Player();
 		Player player2 = new Player(2);
+
+		player1.placeShips(1);
+		player1.printGameBoard();
 		
 	}
-	
-	// print out the current state of the game board (current player cannot see other player ship
-	// locations, only hits, misses, and locations of sunken ships of other player)
-	public static void printGameBoard(int playerNum){
-		
-	}
-	
-	// prompts user to place ships into game board
-	public static void placeShips(int playerNum){
-	
-	Scanner read = new Scanner(System.in);
-	System.out.println("/n Player:" + playerNum + " place your ships");
-	
-	for (int i=1; i <= 5; i++) // 5 ships?
-	{
-		System.out.print("X coordinate for" + i + "ship");
-		int xCord = input.NextInt();
 
-		System.out.print("Y coordinate for" + i + "ship");
-		int yCord = input.NextInt();
 
-		if ((xCord >= 0 && xCord < numRows) && (yCord >= 0 && yCord < numColumns) && (gameboard[xCord][yCord] == " "))
-		{
-			gameboard[xCord][yCord] = "x";
-			System.out.print(i + "your ship was deployed");
-			i++;
-		}
-	}	
-		printGameBoard();
 
-	}
-	
-	// prompts user to select spot on game board to attack 
+	// prompts user to select spot on game board to attack
 	// (make sure to include error checking user selects their own ship or user selects invalid x and y coordinate)
 	// (if user selects correctly, they get another shot at guessing)
 	public static void attack(int playerNum){
-		
+
 	}
 
 
--- Player.java
+++ Player.java
@@ -1,7 +1,8 @@
+import java.util.*;
 
 public class Player {
-	private final int numColumns = 10;
-	private final int numRows = 10;
+	static final int numColumns = 10;
+	static final int numRows = 10;
 
 	public int playerNum;
 
@@ -9,25 +10,24 @@ public class Player {
 	public int[][] guessBoard;
 
 	// 0 = no guess, 1 = correct guess, 2 = incorrect guess, 3 = represent a sunken ship
-	public int[][] gameBoard;
+	static char[][] gameBoard;
 
 	// constructors (int num = player number)
 	public Player(){
 		playerNum = 1;
 		guessBoard = new int[numRows][numColumns];
-		gameBoard = new int[numRows][numColumns];
+		gameBoard = new char[numRows][numColumns];
 		createGameBoard();
 	}
 	public Player(int num){
 		playerNum = num;
 		guessBoard = new int[numRows][numColumns];
-		gameBoard = new int[numRows][numColumns];
+		gameBoard = new char[numRows][numColumns];
 		createGameBoard();
 	}
 
 	// initializes empty game board
-	private static void createGameBoard(){
-	char [][] gameBoard = new char[numColumns][numRows];
+	static void createGameBoard(){
 
 	//print top row of numbers
 	System.out.print(" ");
@@ -50,6 +50,54 @@ public class Player {
 		System.out.println();
 		}
 
+	}
+
+	// prompts user to place ships into game board
+	public static void placeShips(int playerNum){
+
+	Scanner input = new Scanner(System.in);
+	System.out.println("\nPlayer: " + playerNum + " place your ships");
+
+	for (int i=1; i <= 5; i++) // 5 ships?
+	{
+		System.out.print("X coordinate for ship " + i + ": ");
+		int xCord = input.nextInt();
+
+		System.out.print("Y coordinate for ship: " + i + ": ");
+		int yCord = input.nextInt();
+
+		if ((xCord >= 0 && xCord < numRows) && (yCord >= 0 && yCord < numColumns) && (gameBoard[xCord][yCord] == '~'))
+		{
+			gameBoard[xCord][yCord] = 'x';
+			System.out.print("Your ship was deployed\n");
+		}
+	}
+
+	}
+
+	// print out the current state of the game board (current player cannot see other player ship
+	// locations, only hits, misses, and locations of sunken ships of other player)
+	public static void printGameBoard(){
+
+		//print top row of numbers
+		System.out.print(" ");
+		for(int i = 0; i < numColumns; i++)
+		{
+			System.out.print(i);
+		}
+		System.out.println();
+
+		for(int i = 0; i < gameBoard.length; i++)
+		{
+			for(int j = 0; j < gameBoard[i].length; j++)
+			{
+				if (j == 0)
+						System.out.print(i + "|" + gameBoard[i][j]);
+				else
+						System.out.print(gameBoard[i][j]);
+			}
+			System.out.println();
+			}
 
 	}
 
