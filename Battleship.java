a03a494159cbc02ee86759e0e61fe1c7211713ce
AcousticCrunch <hogwarts0500@gmail.com>
2022-04-20 11:08:30 -0400

    4/20 Changes
    Fixed attack, and got Battleship into working order, with the exception of the GUI.

--- Battleship.java
+++ Battleship.java
@@ -8,64 +8,30 @@ public class Battleship {
 		Player player1 = new Player();
 		Player player2 = new Player(2);
 
-		player1.placeShips(1);
-		player1.printGameBoard(1);
-		player2.placeShips(2);
-		player2.printGameBoard(2);
-
-
-		//
-		// do{
-		// 	player1.printGameBoard();
-		// 	player1.attack();
-		//
-		// 	player2.printGameBoard();
-		// 	player2.attack();
-		//
-		// }while(player1Ships != 0 || player2Ships != 0 );
-
+		player1.placeShips();
+		player1.printGameBoard();
+		player2.placeShips();
+		player2.printGameBoard();
+		
+		 do{
+		 	player1.printGameBoard();
+		 	player1.attack(player2);
+		
+		 	player2.printGameBoard();
+		 	player2.attack(player1);
+		
+		 }while(player1.numShips != 0 || player2.numShips != 0 );
+		 
+		 if (player1.numShips == 0)
+			 System.out.println("Congratualtions Player 1! You  Win!");
+		 
+		 if (player2.numShips == 0)
+			 System.out.println("Congratualtions Player 2! You  Win!");
+	
+		 
 	}//end of main
+}//end of Battleship
 
 
 
-	// prompts user to select spot on game board to attack
-	// (make sure to include error checking user selects their own ship or user selects invalid x and y coordinate)
-	// (if user selects correctly, they get another shot at guessing)
 	
-  public static void attack(int playerNum){
-	
-	int xC, yC; // coordinates
-
-	Scanner s = new Scanner(System.in);
-	System.out.print("Enter X coordinate");
-	xC = s.nextInt();
-	System.out.print("Enter Y coordinate");
-	yC = s.nextInt();
-
-	if (guessboard[xC][yC] == "x")
-	{
-		System.out.println("You sunk the ship!");
-		guessboard[xC][yC] = "!";
-		--BattleShip.placeShips;
-	}
-
-	else if (guessboard[xC][yC] == "@")
-	{
-		System.out.println("You sunk your own ship!"); 
-		guessboard[xC][yC] = "x";
-		--BattleShip.placeShips;
-	}
-	
-	else if (guessboard[xC][yC] == " " )
-	{
-		System.out.println("You missed");
-		guessboard[xC][yC] = "-";
-	}
-
-	// makes sure that the ship is on the board
-	else if ((x < 0 || x >= numRows) || (y < 0 || y >= numColumns)
-	{
-		System.out.println("Out of bounds. Try again:);
-	}
-	
-}
--- Player.java
+++ Player.java
@@ -5,9 +5,10 @@ public class Player {
 	static final int numRows = 10;
 
 	public int playerNum;
+	public int numShips = 5;
 
 	// 0 = open ocean, 1 = ship
-	public int[][] guessBoard;
+	public char[][] guessBoard;
 
 	// 0 = no guess, 1 = correct guess, 2 = incorrect guess, 3 = represent a sunken ship
 	//static char[][] gameBoard;
@@ -16,13 +17,13 @@ public class Player {
 	// constructors (int num = player number)
 	public Player(){
 		playerNum = 1;
-		guessBoard = new int[numRows][numColumns];
+		guessBoard = new char[numRows][numColumns];
 		gameBoard = new char[numRows][numColumns];
 		createGameBoard();
 	}
 	public Player(int num){
 		playerNum = num;
-		guessBoard = new int[numRows][numColumns];
+		guessBoard = new char[numRows][numColumns];
 		gameBoard = new char[numRows][numColumns];
 		createGameBoard();
 	}
@@ -30,24 +31,17 @@ public class Player {
 	// initializes empty game board
 	private void createGameBoard(){
 
-		//print top row of numbers
-		System.out.print(" ");
-		for(int i = 0; i < numColumns; i++)
-			System.out.print(i);
-
-		System.out.println();
-
 		for(int i = 0; i < gameBoard.length; i++)
 		{
 			for(int j = 0; j < gameBoard[i].length; j++)
-			{
 				gameBoard[i][j] = '~';
-				if (j == 0)
-						System.out.print(i + "|" + gameBoard[i][j]);
-				else
-						System.out.print(gameBoard[i][j]);
-			}
-			System.out.println();
+		}
+		
+	
+		for (int i = 0; i < guessBoard.length; i++)
+		{
+			for (int j = 0; j < guessBoard[i].length; j++)
+				guessBoard[i][j] = '~';
 		}
 
 	}//end of createGameBoard()
@@ -57,41 +51,92 @@ public class Player {
 
 		//creates gameboard and assigns it accordingly based on playerNum input
 		Scanner input = new Scanner(System.in);
-		System.out.println("\nPlayer place your ships");
+		System.out.println("Player " + playerNum + " please place your ships");
+		int shipsDeployed = 0;
 
-
-		for (int i=1; i <= 5; i++) // 5 ships?
+		do
 		{
-			System.out.print("X coordinate for ship " + i + ": ");
+			System.out.print("X coordinate for ship " + (shipsDeployed + 1) + ": ");
 			int xCord = input.nextInt();
 
-			System.out.print("Y coordinate for ship: " + i + ": ");
+			System.out.print("Y coordinate for ship: " + (shipsDeployed + 1) + ": ");
 			int yCord = input.nextInt();
 
 			if ((xCord >= 0 && xCord < numRows) && (yCord >= 0 && yCord < numColumns) && (gameBoard[xCord][yCord] == '~'))
 			{
 				gameBoard[xCord][yCord] = 'x';
 				System.out.print("Your ship was deployed\n");
+				shipsDeployed++;
 			}
-		}
+			else
+				System.out.println("Coordinates out of bounds. Try again.");
+			
+		}while (shipsDeployed !=  5);
 
 	}//end of placeShips()
 
 
 
 	// prompts user to select spot on game board to attack
-	// (make sure to include error checking user selects their own ship or user selects invalid x and y coordinate)
-	// (if user selects correctly, they get another shot at guessing)
-	public static void attack(int playerNum){
+		// (make sure to include error checking user selects their own ship or user selects invalid x and y coordinate)
+		// (if user selects correctly, they get another shot at guessing)
+		
+	  public void attack(Player enemy){
+		
+		int xC, yC; // coordinates
 
+		Scanner s = new Scanner(System.in);
+		printGuessBoard();
+		System.out.print("Player " + playerNum + " please enter X coordinate: ");
+		xC = s.nextInt();
+		System.out.print("Player " + playerNum + "  please enter Y coordinate: ");
+		yC = s.nextInt();
+
+		// makes sure that the ship is on the board
+		if ((xC < 0 || xC >= numRows) || (yC < 0 || yC >= numColumns))
+		{
+			System.out.println("Out of bounds. Try again.");
+			this.attack(enemy);
+		}
+		
+		// Enemy ship is already sunk
+		else if (enemy.gameBoard[xC][yC] == '@')
+		{
+			System.out.println("Ship already sunk. Try again.");
+			this.attack(enemy);
+		}
+		
+		// Player gave correct guess
+		else if (enemy.gameBoard[xC][yC] == 'x')
+		{
+			System.out.println("You sunk the ship!");
+			guessBoard[xC][yC] = '!';
+			enemy.gameBoard[xC][yC]  =  '@';
+			enemy.numShips--;
+			
+			if (enemy.numShips != 0)
+				this.attack(enemy);
+		}
+		
+		// Player gave incorrect guess
+		else if (enemy.gameBoard[xC][yC] == '~')
+		{
+			System.out.println("You missed");
+			guessBoard[xC][yC] = '-';
+			printGuessBoard();
+		}
+	
 	}//end of attack()
 
 
-	// print out the current state of the game board (current player cannot see other player ship
-	// locations, only hits, misses, and locations of sunken ships of other player)
+
+	// print out the current state of the game board
 	public void printGameBoard(){
-
-
+		
+		System.out.println("\n-----Player "  + playerNum + "-----");
+		System.out.println("----Game Board----");
+		System.out.println();
+		
 		//print top row of numbers
 		System.out.print("  ");
 		for(int i = 0; i < numColumns; i++)
@@ -99,7 +144,7 @@ public class Player {
 			System.out.print(i);
 		}
 		System.out.println();
-
+		
 		for(int i = 0; i < gameBoard.length; i++)
 		{
 			for(int j = 0; j < gameBoard[i].length; j++)
@@ -111,7 +156,35 @@ public class Player {
 			}
 			System.out.println();
 		}
-
-	}//end of print
+		System.out.println();
+		
+	}//end of print()
+	
+	public void printGuessBoard() {
+		System.out.println("\n-----Player "  + playerNum + "-----");
+		System.out.println("----Guess Board----");
+		System.out.println();
+		
+		//print top row of numbers
+		System.out.print("  ");
+		for(int i = 0; i < numColumns; i++)
+		{
+			System.out.print(i);
+		}
+		System.out.println();
+		
+		for(int i = 0; i < guessBoard.length; i++)
+		{
+			for(int j = 0; j < guessBoard[i].length; j++)
+			{
+				if (j == 0)
+						System.out.print(i + "|" + guessBoard[i][j]);
+				else
+						System.out.print(guessBoard[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println(); System.out.println();
+	}//end of printGuessBoard()
 
 }//end of player
